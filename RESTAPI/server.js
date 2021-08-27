5e1682bdc31267c8aa4da23379107481c91f4989
= <=>
2021-08-27 16:53:09 +0700

    update server

--- RESTAPI/server.js
+++ RESTAPI/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();  
 var fs = require("fs");  
 var bodyParser = require('body-parser');  
-
+// var formidable = require('formidable');
 /// câu lệnh chạy server : node server.js
 
 //enable CORS for request verbs
@@ -11,7 +11,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");  
   res.header("Access-Control-Allow-Methods","POST, GET, PUT, DELETE, OPTIONS");  
   next();  
-});  
+}); 
 app.use(bodyParser.urlencoded({  
     extended: true  
 }));  
@@ -28,15 +28,19 @@ app.post('/product', function (req, res) {
     fs.readFile( `${__dirname}/data/product.json`, 'utf8', function (err, data) {  
         data = JSON.parse(data);
         var max = 0;
-        var max = data.length === 0 ? 0 : data.reduce(function(prev, current) {
-            return (prev.Id > current.Id) ? prev.Id : current.Id
-        });
+        if(data.length >0){
+            var maxObject = data.reduce(function(prev, current) {
+                return (prev.Id > current.Id) ? prev : current
+            });
+            max = maxObject.Id;
+        }
+        
         var newproduct = {Id:max+1,...req.body};
         data.push(newproduct);
         fs.writeFile(`${__dirname}/data/product.json`, JSON.stringify(data), function (err) {
             if (err) return console.log(err);
             console.log('Create product success.');
-        }); 
+        });
         res.end(JSON.stringify(data));  
     });  
 })  
@@ -60,7 +64,7 @@ app.put('/product/:id', function(req,res){
         fs.writeFile(`${__dirname}/data/product.json`, JSON.stringify(data), function (err) {
             if (err) return console.log(err);
             console.log('Update product success.');
-        }); 
+        });
         res.end(JSON.stringify(data));  
     });  
 });
@@ -143,6 +147,49 @@ app.delete('/customer/:id', function (req, res) {
     });  
 })  
 //#endregion
+
+//#region 
+app.get('/customer', function (req, res) {  
+    fs.readFile( `${__dirname}/data/customer.json`, 'utf8', function (err, data) {   
+        res.end(JSON.stringify(JSON.parse(data),null,2));  
+    });  
+});
+
+app.post('/customer', function (req, res) {  
+    fs.readFile( `${__dirname}/data/customer.json`, 'utf8', function (err, data) {  
+        data = JSON.parse(data);
+        var max = 0;
+        var max = data.length === 0 ? 0 : data.reduce(function(prev, current) {
+            return (prev.Id > current.Id) ? prev.Id : current.Id
+        });
+        var newproduct = {Id:max+1,...req.body};
+        data.push(newproduct);
+        fs.writeFile(`${__dirname}/data/customer.json`, JSON.stringify(data), function (err) {
+            if (err) return console.log(err);
+            console.log('Create product success.');
+        }); 
+        res.end(JSON.stringify(data));  
+    });  
+})  
+//#endregion
+
+app.post('/auth', function (req, res) {  
+    fs.readFile( `${__dirname}/data/customer.json`, 'utf8', function (err, data) {  
+        data = JSON.parse(data);
+        var max = 0;
+        var max = data.length === 0 ? 0 : data.reduce(function(prev, current) {
+            return (prev.Id > current.Id) ? prev.Id : current.Id
+        });
+        var newproduct = {Id:max+1,...req.body};
+        data.push(newproduct);
+        fs.writeFile(`${__dirname}/data/customer.json`, JSON.stringify(data), function (err) {
+            if (err) return console.log(err);
+            console.log('Create product success.');
+        }); 
+        res.end(JSON.stringify(data));  
+    });  
+})
+
 
 var server = app.listen(8081, function () {  
   var host = server.address().address  
